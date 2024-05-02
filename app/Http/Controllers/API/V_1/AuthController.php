61abf03339d33e849114f4dd9f416608fbffb927
Foster Amponsah Asante <fostersnt@gmail.com>
2024-05-02 09:21:21 +0000

    Added register method to the AuthController in API version 1

--- app/Http/Controllers/API/V_1/AuthController.php
+++ app/Http/Controllers/API/V_1/AuthController.php
@@ -2,10 +2,48 @@
 
 namespace App\Http\Controllers\API\V_1;
 
+use App\Helpers\General;
 use App\Http\Controllers\Controller;
+use App\Models\ApiUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('api')->except(['register']);
+    }
+
+    public function register(Request $request)
+    {
+        $rules = [
+            'company_name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+        ];
+
+        $messages = [
+            'company_name.required' => 'Company name is empty',
+            'email.required' => 'Email is empty',
+            'email.email' => 'Invalid email',
+            'password.min' => 'Password is less than 8 characters'
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return General::api_failure_response($validator->errors()->all(), 'Invalid data');
+        } else {
+            $api_user = ApiUser::query()->create([
+                'company_name' => $request->company_name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+
+            return General::api_success_response($api_user, 'Invalid data');
+        }
+
+    }
 }
